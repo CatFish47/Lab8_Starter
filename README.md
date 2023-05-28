fb6e6136724011f1b0e94460008f80cd9f4ddd37
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2023-05-27 23:05:34 -0700

    Completed expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+
+Grant Cheng
+
+1. Automated tests should be ran in a Github action that runs whenever the code is pushed. That way, if something is broken, Github actions will keep a log of exactly which commit broke the tests, allowing for the bug to be easily traceable. Preferably, people will manually run the tests on their own machines as well, but not only is that not 100% reliable (because people will forget to run tests before pushing code sometimes), having it in the CI is a lot more safer.
+2. No, because end-to-end testing tests the entire app as a whole. To check a specific function, you would use unit testing, which tests individual components of your app (like a function).
+3. No, because the message feature of an app is a relatively large-scale process that can require multiple steps and have multiple components interact with each other. This feature would be more suitable for an end-to-end test.
+4. Yes, because this is a small scale feature that can be easily tested without having to test the interaction of multiple components.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,91 @@
 // unit.test.js
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
+const { isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor } = functions;
 
 // TODO - Part 2
+
+// Tests isPhoneNumber
+test('Tests that (666) 666-6666 is a valid phone number', () => {
+    expect(isPhoneNumber('(666) 666-6666')).toBe(true);
+});
+
+test('Tests that 123-456-7890 is a valid phone number', () => {
+    expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('Tests that +1 1234567890 is not a valid phone number', () => {
+    expect(isPhoneNumber('+1 1234567890')).toBe(false);
+});
+
+test('Tests that 1234567890 is not a valid phone number', () => {
+    expect(isPhoneNumber('1234567890')).toBe(false);
+});
+
+// Tests isEmail
+test('Tests that gxcheng@ucsd.edu is a valid email', () => {
+    expect(isEmail('gxcheng@ucsd.edu')).toBe(true);
+});
+
+test('Tests that a@b.co is a valid email', () => {
+    expect(isEmail('a@b.co')).toBe(true);
+});
+
+test('Tests that @@.. is not a valid email', () => {
+    expect(isEmail('@@..')).toBe(false);
+});
+
+test('Tests that wee@geee.commm is not a valid email', () => {
+    expect(isEmail('wee@geee.commm')).toBe(false);
+});
+
+// Tests isStrongPassword
+test('Tests that a39123da is a valid password', () => {
+    expect(isStrongPassword('a39123da')).toBe(true);
+});
+
+test('Tests that a______b23 is a valid password', () => {
+    expect(isStrongPassword('a______b23')).toBe(true);
+});
+
+test('Tests that a@fad2tr/] is not a valid password', () => {
+    expect(isStrongPassword('a@fad2tr/]')).toBe(false);
+});
+
+test('Tests that 3aaeda2 is not a valid password', () => {
+    expect(isStrongPassword('3aaeda2')).toBe(false);
+});
+
+// Tests isDate
+test('Tests that 4/7/2003 is a valid date', () => {
+    expect(isDate('4/7/2003')).toBe(true);
+});
+
+test('Tests that 12/31/2023 is a valid date', () => {
+    expect(isDate('12/31/2023')).toBe(true);
+});
+
+test('Tests that 1/2/3 is not a valid date', () => {
+    expect(isDate('1/2/3')).toBe(false);
+});
+
+test('Tests that 04-07-2003 is not a valid date', () => {
+    expect(isDate('04-07-2003')).toBe(false);
+});
+
+// Tests isHexColor
+test('Tests that #FFF is a valid hex color', () => {
+    expect(isHexColor('#FFF')).toBe(true);
+});
+
+test('Tests that aF2B2e is a valid hex color', () => {
+    expect(isHexColor('aF2B2e')).toBe(true);
+});
+
+test('Tests that #abcd is not a valid hex color', () => {
+    expect(isHexColor('#abcd')).toBe(false);
+});
+
+test('Tests that efg is not a valid hex color', () => {
+    expect(isHexColor('efg')).toBe(false);
+});
